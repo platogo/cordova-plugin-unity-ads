9f403333909116bf00f99f1e0216caafc07bf6f4
Christoph Altmann <christoph.altmann@platogo.com>
2019-10-04 18:16:04 +0200

    Add basic iOS implementation

--- src/ios/UnityAdsPlugin.h
+++ src/ios/UnityAdsPlugin.h
@@ -1,8 +1,59 @@
+#import "UnityAdsPlugin.h"
 #import <Cordova/CDVPlugin.h>
+#import <UnityAds/UnityAds.h>
 
-@interface UnityAdsPlugin : CDVPlugin
+@implementation UnityAdsPlugin
 
-- (void) initialize:(CDVInvokedUrlCommand *)command;
-- (void) show:(CDVInvokedUrlCommand *)command;
+@synthesize initializeCallbackId;
+@synthesize showCallbackId;
+
+- (void)initialize:(CDVInvokedUrlCommand*)command
+{
+    NSString* gameId = [command.arguments objectAtIndex:0];
+
+    self.initializeCallbackId = command.callbackId;
+    ViewController* vc = [ViewController alloc];
+    [vc initialize:self];
+    [UnityAds initialize:gameId delegate:[vc self]];
+}
+
+- (void)show:(CDVInvokedUrlCommand*)command
+{
+    self.showCallbackId = command.callbackId;
+    
+    [UnityAds show:self.viewController];
+}
+
+@end
+
+@implementation ViewController
+
+@synthesize unityAdsPlugin;
+
+- (void)initialize:(UnityAdsPlugin *)unityAdsPlugin_ {
+    self.unityAdsPlugin = unityAdsPlugin_;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)unityAdsDidError:(UnityAdsError)error withMessage:(nonnull NSString *)message {
+
+}
+
+- (void)unityAdsDidFinish:(nonnull NSString *)placementId withFinishState:(UnityAdsFinishState)state {
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];;
+    [unityAdsPlugin.commandDelegate sendPluginResult:pluginResult callbackId:unityAdsPlugin.showCallbackId];
+}
+
+- (void)unityAdsDidStart:(nonnull NSString *)placementId {
+
+}
+
+- (void)unityAdsReady:(nonnull NSString *)placementId {
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];;
+    [unityAdsPlugin.commandDelegate sendPluginResult:pluginResult callbackId:unityAdsPlugin.initializeCallbackId];
+}
 
 @end
--- src/ios/UnityAdsPlugin.m
+++ src/ios/UnityAdsPlugin.m
@@ -4,32 +4,67 @@
 
 @implementation UnityAdsPlugin
 
+@synthesize initializeCallbackId;
+@synthesize showCallbackId;
+
 - (void)initialize:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
+    NSString* gameId = [command.arguments objectAtIndex:0];
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    self.initializeCallbackId = command.callbackId;
+    ViewController* vc = [ViewController alloc];
+    [vc initialize:self];
+    [UnityAds initialize:gameId delegate:[vc self]];
 }
 
 - (void)show:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
+    self.showCallbackId = command.callbackId;
+    
+    [UnityAds show:self.viewController];
+}
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+@end
+
+@implementation ViewController
+
+@synthesize unityAdsPlugin;
+
+- (void)initialize:(UnityAdsPlugin *)unityAdsPlugin_ {
+    self.unityAdsPlugin = unityAdsPlugin_;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)unityAdsDidError:(UnityAdsError)error withMessage:(nonnull NSString *)message {
+
+}
+
+- (void)unityAdsDidFinish:(nonnull NSString *)placementId withFinishState:(UnityAdsFinishState)state {
+    if (state == kUnityAdsFinishStateCompleted) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];;
+        [unityAdsPlugin.commandDelegate sendPluginResult:pluginResult callbackId:unityAdsPlugin.showCallbackId];
     }
+}
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+- (void)unityAdsDidStart:(nonnull NSString *)placementId {
+
+}
+
+- (void)unityAdsReady:(nonnull NSString *)placementId {
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];;
+    [unityAdsPlugin.commandDelegate sendPluginResult:pluginResult callbackId:unityAdsPlugin.initializeCallbackId];
+}
+
+- (void)unityAdsDidClick:(nonnull NSString *)placementId {
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];;
+    [unityAdsPlugin.commandDelegate sendPluginResult:pluginResult callbackId:unityAdsPlugin.showCallbackId];
+}
+
+- (void)unityAdsPlacementStateChanged:(nonnull NSString *)placementId oldState:(UnityAdsPlacementState)oldState newState:(UnityAdsPlacementState)newState {
+    
 }
 
 @end
