73bf751a2064335dd9a80bdef52d9cc1e602fe7d
Christoph Altmann <christoph.altmann@platogo.com>
2020-06-10 13:10:03 +0200

    Do not use js arrow functions as they are not supported by iOS 9

--- www/unity-ads.js
+++ www/unity-ads.js
@@ -2,14 +2,14 @@ var exec = require('cordova/exec');
 
 function UnityAds() { }
 
-UnityAds.prototype.initialize = (gameId, testMode, debugMode) => {
-    return new Promise((resolve, reject) => {
+UnityAds.prototype.initialize = function (gameId, testMode, debugMode) {
+    return new Promise(function (resolve, reject) {
         exec(resolve, reject, 'UnityAdsPlugin', 'initialize', [gameId, testMode, debugMode]);
     })
 }
 
-UnityAds.prototype.show = (serverId, videoAdPlacementId) => {
-    return new Promise((resolve, reject) => {
+UnityAds.prototype.show = function (serverId, videoAdPlacementId) {
+    return new Promise(function (resolve, reject) {
         exec(resolve, reject, 'UnityAdsPlugin', 'show', [serverId, videoAdPlacementId]);
     })
 }
